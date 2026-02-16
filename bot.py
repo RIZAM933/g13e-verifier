038d9dd9e491137a77ab38d503b88e2f339796bb
Rizam <valievimamutdin9@gmail.com>
2026-02-16 14:40:42 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+import os
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} запущен")
+
+bot.run(TOKEN)
